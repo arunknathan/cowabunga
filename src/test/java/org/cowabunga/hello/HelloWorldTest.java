eba05ff6d0af5d0e982921af8fa4f63202aba7bf
Arunamaru <arunknathan@gmail.com>
2017-03-21 11:46:02 +0530

    using gradle wrapper

---
+++ src/test/java/org/cowabunga/hello/HelloWorldTest.java
@@ -0,0 +1,18 @@
+package org.cowabunga.hello;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void testMain() {
+		try {
+			HelloWorld.main(null);
+		} catch (Exception ex) {
+			fail("HelloWorld main method did not execute completely - an Exception was thrown.");
+		}
+	}
+
+}
